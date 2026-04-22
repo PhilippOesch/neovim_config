090cb3880a7035488a126c1fed263e250ed3e4ce
Philipp Oeschger <oeschger@babtec.de>
2026-04-22 13:01:43 +0200

    update config to get todo path from global prop

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -2,7 +2,7 @@
 return {
 	init = function()
 		local todo = require("plugins.todo.init")
-		todo.setup()
+		todo.setup({ todo_file = vim.g.todo_path or require("plugins.todo.config").get_default_config().todo_file })
 
 		vim.keymap.set("n", "<leader>do", function()
 			todo.toggle()
