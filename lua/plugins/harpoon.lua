e0029c498724a5c41187ee19637fa7a10c937936
Philipp Oeschger <philippoeschger@Philipps-MacBook-Air.local>
2026-08-16 14:14:16 +0200

    change harpoon default binding

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,7 +13,7 @@ return {
 		local wk = require("which-key")
 		--
 		-- keymaps
-		vim.keymap.set("n", "<leader>H", function()
+		vim.keymap.set("n", "<ctrl>h", function()
 			require("harpoon.ui").toggle_quick_menu()
 		end, {
 			noremap = true,
